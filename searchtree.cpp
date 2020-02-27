da15b7c191e05fb7807fa5ddc17aac057db8ddbc
Novawolf127 <43829470+Novawolf127@users.noreply.github.com>
2020-02-26 21:41:05 -0500

    made first draft versions of insert, remove, and lookup. Also modified node class to hold route and base node fields

--- searchtree.cpp
+++ searchtree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "treap.h"
 
 using namespace std;
 
@@ -7,21 +8,32 @@ class SearchTree {
 private:
     class Node {
     public:
-        T val;
+        bool isRoute {true};
+		T val;
+		int weight; 
+		Treap* treap {NULL};
+		
         Node *left {NULL};
         Node *right {NULL};
 
         Node(T val) {
             this->val = val;
+			this->weight = weightDist(randEngine);
         }
     };
 
     Node *head {NULL};
+	
 public:
     void insert(T val) {
-        Node *n = new Node<T>(val);
-
         if (head == NULL) {
+            Node *n = new Node(val);
+			
+			n->treap = new Treap();
+			n->treap.immutableInsert(val);
+			n->val = NULL;
+			n->isRoute = false;
+			
             head = n;
             return;
         }
@@ -29,8 +41,22 @@ public:
         Node *temp = head;
 
         while (true) {
+			if (temp.isRoute == false)
+			{
+				temp->treap.immutableInsert(val);
+				return;
+			}
+			
+			
             if (temp->val > val) {
                 if (temp->left == NULL) {
+					Node *n = new Node(val);
+					
+					n->treap = new Treap();
+					n->treap.immutableInsert(val);
+					n->val = NULL;
+					n->isRoute = false;
+										
                     temp->left = n;
                     return;
                 }
@@ -40,6 +66,13 @@ public:
             }
             else {
                 if (temp->right == NULL) {
+					Node *n = new Node(val);
+					
+					n->treap = new Treap();
+					n->treap.immutableInsert(val);
+					n->val = NULL;
+					n->isRoute = false;
+					
                     temp->right = n;
                     return;
                 }
@@ -51,46 +84,75 @@ public:
     }
 
     void remove(T val) {
+		if (head == NULL) {
+            return;
+        }
+
         Node *temp = head;
-        
+
         while (true) {
+			if (temp.isRoute == false)
+			{
+				temp->treap.immutableRemove(val);
+				return;
+			}
+			
+			
             if (temp->val > val) {
-                temp = temp->left;
-            }           
-            else if (temp->val < val) {
-                temp = temp->right;
+                if (temp->left == NULL) {
+                    return;
+                }
+                else {
+                    temp = temp->left;
+                }
             }
-            else if (temp->val == val) {
-                
-            }
-            else if (temp == NULL) {
-                return NULL;
+            else {
+                if (temp->right == NULL) {
+                    return;
+                }
+                else {
+                    temp = temp->right;
+                }
             }
         }
     }
 
     bool lookup(T val) {
+		if (head == NULL) {
+            return false;
+        }
+
         Node *temp = head;
-        
+
         while (true) {
+			if (temp.isRoute == false)
+			{
+				return temp->treap.contains(val);
+			}
+			
+			
             if (temp->val > val) {
-                temp = temp->left;
-            }           
-            else if (temp->val < val) {
-                temp = temp->right;
+                if (temp->left == NULL) {
+                    return false;
+                }
+                else {
+                    temp = temp->left;
+                }
             }
-            else if (temp->val == val) {
-                return true
-            }
-            else if (temp == NULL) {
-                return false;
+            else {
+                if (temp->right == NULL) {
+                    return false;
+                }
+                else {
+                    temp = temp->right;
+                }
             }
         }
-    
-        
-    }
+	}
 };
 
-int main(void) {
-    
+int main () {
+	
+	return 0;
 }
+
