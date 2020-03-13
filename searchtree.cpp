b34721f2418bdde28b877dd6d222f77761d94b4a
Benjamin Le Heup <37462665+bleheup@users.noreply.github.com>
2020-03-12 21:48:47 -0400

    Add files via upload

--- searchtree.cpp
+++ searchtree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "treap.h"
+#include "treap.cpp"
 
 using namespace std;
 
@@ -9,9 +10,9 @@ private:
     class Node {
     public:
         bool isRoute {true};
-		T val;
+	       T val;
 		Treap* treap {NULL};
-		
+
         Node *left {NULL};
         Node *right {NULL};
 
@@ -22,17 +23,24 @@ private:
     };
 
     Node *head {NULL};
-	
+
 public:
+
+    SearchTree(T val)
+    {
+       Node *head = NULL;
+    }
+
+
     void insert(T val) {
         if (head == NULL) {
             Node *n = new Node(val);
-			
+
 			n->treap = new Treap();
 			n->treap.immutableInsert(val);
 			n->val = NULL;
 			n->isRoute = false;
-			
+
             head = n;
             return;
         }
@@ -40,8 +48,8 @@ public:
         Node *temp = head;
 
         while (true) {
-			// Travels until it finds the base node where the value should go 
-			if (temp.isRoute == false)
+			// Travels until it finds the base node where the value should go
+			if (temp->isRoute == false)
 			{
 				temp->treap.immutableInsert(val);
 				// If inserting causes the treap to become too large, it splits into two.
@@ -50,35 +58,35 @@ public:
 					Node *left = new Node(val);
 					left->val = NULL;
 					left->isRoute = false;
-					
+
 					Node *right = new Node(val);
 					right->val = NULL;
 					right->isRoute = false;
-					
+
 					int headval = temp->treap.nodes[temp->treap.root].val;
-					
+
 					temp->treap.split(left->treap, right->treap);
-					
+
 					temp->val = headval;
 					temp->isRoute = true;
 					temp->left = left;
 					temp->right = right;
-					// Leave this node's treap in the struct. 
+					// Leave this node's treap in the struct.
 				}
 				return;
 			}
-			
-			
+
+
             if (temp->val > val) {
                 if (temp->left == NULL) {
 					// Inserts a new node at this spot if there is no base node here for it.
 					Node *n = new Node(val);
-					
+
 					n->treap = new Treap();
 					n->treap.immutableInsert(val);
 					n->val = NULL;
 					n->isRoute = false;
-										
+
                     temp->left = n;
                     return;
                 }
@@ -90,12 +98,12 @@ public:
                 if (temp->right == NULL) {
 					// Inserts a new node at this spot if there is no base node here for it.
 					Node *n = new Node(val);
-					
+
 					n->treap = new Treap();
 					n->treap.immutableInsert(val);
 					n->val = NULL;
 					n->isRoute = false;
-					
+
                     temp->right = n;
                     return;
                 }
@@ -116,13 +124,13 @@ public:
 
         while (true) {
 			// Travels until it finds the base node with the value and performs remove on the treap inside.
-			if (temp.isRoute == false)
+			if (temp->isRoute == false)
 			{
 				temp->treap.immutableRemove(val);
-				
+
 				// If that would cause the treap to become too small it performs a merge with temp's sibling.
 				if (temp->treap.getSize() <= 16)
-				{										
+				{
 					if (temp2->left == temp)
 					{
 						if (temp2->right != NULL && temp2->right->treap.getSize() <= 16)
@@ -130,20 +138,20 @@ public:
 							temp->treap = temp->treap.merge(temp, temp2->right);
 						}
 					}
-						
+
 					else
 					{
 						if (temp2->left != NULL && temp2->left->treap.getSize() <= 16)
 						{
 							temp->treap = temp->treap.merge(temp2->left, temp);
 						}
-							
+
 					}
 				}
 				return;
 			}
-			
-			
+
+
             if (temp->val > val) {
                 if (temp->left == NULL) {
                     return;
@@ -174,12 +182,12 @@ public:
 
         while (true) {
 			// Travels down until it finds the correct base node, performs contains on the treap once it does.
-			if (temp.isRoute == false)
+			if (temp->isRoute == false)
 			{
 				return temp->treap.contains(val);
 			}
-			
-			
+
+
             if (temp->val > val) {
                 if (temp->left == NULL) {
                     return false;
@@ -198,12 +206,12 @@ public:
             }
         }
 	}
-	
+
 	vector<T> rangeQuery(T low, T high) {
 		vector<T> result;
 		if (head == NULL) {
             return result;
-        }		
+        }
 
 		vector<Node> nodesToCheck;
 		nodesToCheck.push_back(head);
@@ -212,9 +220,9 @@ public:
 		while (!nodesToCheck.empty()) {
 			Node *temp = nodesToCheck.back();
 			nodesToCheck.pop_back();
-			
+
 			// If popped node is base node, perform range query on treap and add it to result.
-			if (temp.isRoute == false) {
+			if (temp->isRoute == false) {
 				values = temp->treap.rangeQuery(low, high);
 				result.insert(result.end(), values.begin(), values.end());
 				continue;
@@ -223,32 +231,31 @@ public:
 			Node* currentLeft = temp->left;
 			Node* currentRight = temp->right;
 
-			// Adds left child if within range 
-			if (currentLeft != NULL && temp.val >= low) {
+			// Adds left child if within range
+			if (currentLeft != NULL && temp->val >= low) {
 				nodesToCheck.push_back(currentLeft);
 			}
-			// Adds right child if within range 
-			if (currentRight != NULL && temp.val <= high) {
+			// Adds right child if within range
+			if (currentRight != NULL && temp->val <= high) {
 				nodesToCheck.push_back(currentRight);
 			}
 			// Catches minimum boundary case
-			if (currentLeft != NULL && currentRight != NULL && currentLeft.val <= low && currentRight.val >= low) {
+			if (currentLeft != NULL && currentRight != NULL && currentLeft->val <= low && currentRight->val >= low) {
 				nodesToCheck.push_back(currentLeft->right);
 			}
 			// Catches maximum boundary case
-			if (currentLeft != NULL && currentRight != NULL && currentLeft.val <= high && currentRight.val >= high) {
+			if (currentLeft != NULL && currentRight != NULL && currentLeft->val <= high && currentRight->val >= high) {
 				nodesToCheck.push_back(currentRight->left);
 			}
-			
+
 		}
 		return result;
-		
+
 	}
 
 };
 
-int main () {
-	
-	return 0;
-}
+/*int main () {
 
+	return 0;
+}*/
