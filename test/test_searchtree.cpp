f8e3adcb84ee15cbd08383df0a4c3ba6cbea323c
kboyles8 <boyleskevin8@gmail.com>
2020-03-15 03:06:13 -0400

    Clean up SearchTree tests

--- test/test_searchtree.cpp
+++ test/test_searchtree.cpp
@@ -14,76 +14,61 @@ TEST_F(SearchTreeTest, InsertAndRemoveAndLookup) {
     EXPECT_TRUE(searchtree.lookup(1));
 
     searchtree.insert(2);
-    EXPECT_TRUE(searchtree.lookup(1));
-    searchtree.insert(3);
-    EXPECT_TRUE(searchtree.lookup(1));
-    searchtree.insert(4);
-    EXPECT_TRUE(searchtree.lookup(1));
-    searchtree.insert(5);
-    EXPECT_TRUE(searchtree.lookup(1));
-
-    searchtree.remove(1);
-    EXPECT_FALSE(searchtree.lookup(1));
-
     EXPECT_TRUE(searchtree.lookup(2));
-    searchtree.remove(1);
+    searchtree.insert(3);
     EXPECT_TRUE(searchtree.lookup(3));
-    searchtree.remove(1);
+    searchtree.insert(4);
     EXPECT_TRUE(searchtree.lookup(4));
-    searchtree.remove(1);
+    searchtree.insert(5);
     EXPECT_TRUE(searchtree.lookup(5));
+
     searchtree.remove(1);
     EXPECT_FALSE(searchtree.lookup(1));
+    searchtree.remove(2);
+    EXPECT_FALSE(searchtree.lookup(2));
+    searchtree.remove(3);
+    EXPECT_FALSE(searchtree.lookup(3));
+    searchtree.remove(4);
+    EXPECT_FALSE(searchtree.lookup(4));
+    searchtree.remove(5);
+    EXPECT_FALSE(searchtree.lookup(5));
 }
 
 TEST_F(SearchTreeTest, RangeQuery) {
-    searchtree.insert(1);
-    searchtree.insert(2);
-    searchtree.insert(3);
-    searchtree.insert(4);
-    searchtree.insert(5);
-    searchtree.insert(6);
-    searchtree.insert(7);
-    searchtree.insert(8);
-    searchtree.insert(9);
-
-    vector<int> test = {3, 4, 5, 6, 7, 8, 9};
-    vector<int> test2 = searchtree.rangeQuery(3, 100);
-    sort(test2.begin(), test2.end());
-
-    EXPECT_EQ(test, test2);
-
-    test = {1, 2, 3, 4};
-    test2 = searchtree.rangeQuery(-100, 4);
-    sort(test2.begin(), test2.end());
-
-    EXPECT_EQ(test, test2);
-
-    test = {4, 5, 6};
-    test2 = searchtree.rangeQuery(4, 6);
-    sort(test2.begin(), test2.end());
-
-    EXPECT_EQ(test, test2);
-}
-
-TEST_F(SearchTreeTest, SplitAndMergeBulkTest) {
-    for (int i = 0; i < 1024; i++)
-    {
+    for (int i = 1; i <= 9; i++) {
         searchtree.insert(i);
     }
 
-    for (int i = 0; i < 1024; i++)
-    {
+    vector<int> expectedQuery = {3, 4, 5, 6, 7, 8, 9};
+    vector<int> actualQuery = searchtree.rangeQuery(3, 100);
+    sort(actualQuery.begin(), actualQuery.end());
+    EXPECT_EQ(expectedQuery, actualQuery);
+
+    expectedQuery = {1, 2, 3, 4};
+    actualQuery = searchtree.rangeQuery(-100, 4);
+    sort(actualQuery.begin(), actualQuery.end());
+    EXPECT_EQ(expectedQuery, actualQuery);
+
+    expectedQuery = {4, 5, 6};
+    actualQuery = searchtree.rangeQuery(4, 6);
+    sort(actualQuery.begin(), actualQuery.end());
+    EXPECT_EQ(expectedQuery, actualQuery);
+}
+
+TEST_F(SearchTreeTest, SplitAndMergeBulkTest) {
+    for (int i = 0; i < 1024; i++) {
+        searchtree.insert(i);
+    }
+
+    for (int i = 0; i < 1024; i++) {
         EXPECT_TRUE(searchtree.lookup(i));
     }
 
-    for (int i = 0; i < 1024; i++)
-    {
+    for (int i = 0; i < 1024; i++) {
         searchtree.remove(i);
     }
 
-    for (int i = 0; i < 1024; i++)
-    {
+    for (int i = 0; i < 1024; i++) {
         EXPECT_FALSE(searchtree.lookup(i));
     }
 }
