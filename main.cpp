d553ba358b384606bf6b1c408619946845c9bb60
Tam <tamtheguy@gmail.com>
2020-03-16 17:31:16 -0400

    made first version of the main file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 
 #include "treap.h"
+#include "searchtree.h"
+
+#define NUM_THREADS 4
+#define PARALLEL_START 0
+#define PARALLEL_END 10000
 
 using namespace std;
 
+static void mixedThread(SearchTree *tree, int start, int end) {
+	int op, val;
+	for (int i = start; i < end; i++)
+	{
+		op = rand()%4;
+		val = rand()%end;
+		if (op%4 == 0)
+		{
+			tree->insert(val);
+		}
+		else if (op%4 == 1)
+		{
+			tree->remove(val);
+		}
+		else if (op%4 == 2)
+		{
+			tree->lookup(val);
+		}
+		else
+		{
+			tree->rangeQuery(0, val);
+		}
+
+	}
+}
+
 int main(void) {
-    Treap treap;
+	srand(time(NULL));
+    vector<thread> threads;
+	SearchTree searchtree;
 
-    int size = treap.getSize();
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads.push_back(thread(mixedThread, &searchtree, PARALLEL_START + i, PARALLEL_END));
+    }
 
-    cout << "The treap has " << to_string(size) << " nodes" << endl;
+    for (int i = 0; i < NUM_THREADS; i++) {
+        threads.at(i).join();
+    }
 }
