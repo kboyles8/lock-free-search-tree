ce6815abdfcc5dcc97391ca7ef2414bf194662dc
kboyles8 <boyleskevin8@gmail.com>
2020-03-17 19:58:52 -0400

    Fix indentation

--- main.cpp
+++ main.cpp
@@ -10,35 +10,35 @@
 using namespace std;
 
 static void mixedThread(SearchTree *tree, int start, int end) {
-	int op, val;
-	for (int i = start; i < end; i++)
-	{
-		op = rand()%4;
-		val = rand()%end;
-		if (op%4 == 0)
-		{
-			tree->insert(val);
-		}
-		else if (op%4 == 1)
-		{
-			tree->remove(val);
-		}
-		else if (op%4 == 2)
-		{
-			tree->lookup(val);
-		}
-		else
-		{
-			tree->rangeQuery(0, val);
-		}
+    int op, val;
+    for (int i = start; i < end; i++)
+    {
+        op = rand()%4;
+        val = rand()%end;
+        if (op%4 == 0)
+        {
+            tree->insert(val);
+        }
+        else if (op%4 == 1)
+        {
+            tree->remove(val);
+        }
+        else if (op%4 == 2)
+        {
+            tree->lookup(val);
+        }
+        else
+        {
+            tree->rangeQuery(0, val);
+        }
 
-	}
+    }
 }
 
 int main(void) {
-	srand(time(NULL));
+    srand(time(NULL));
     vector<thread> threads;
-	SearchTree searchtree;
+    SearchTree searchtree;
 
     for (int i = 0; i < NUM_THREADS; i++) {
         threads.push_back(thread(mixedThread, &searchtree, START, END));
