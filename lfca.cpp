bf8b2486b952622c13b30f3cb63103cea108fdc7
kboyles8 <boyleskevin8@gmail.com>
2020-03-27 22:26:46 -0400

    Add constructors and copy-constructors to node hierarchy

--- lfca.cpp
+++ lfca.cpp
@@ -44,12 +44,28 @@ struct route_node {
     atomic<node *> right;             // >= key
     atomic<bool> valid{true};         // Used for join
     atomic<node *> join_id{nullptr};  // ...
+
+    route_node() { }
+    route_node(const route_node &other) {
+        key = other.key;
+        left.store(other.left.load());
+        right.store(other.right.load());
+        valid.store(other.valid.load());
+        join_id.store(other.join_id.load());
+    }
 };
 
 struct normal_base {
     treap *data = nullptr;   // Items in the set
     int stat = 0;            // Statistics variable
     node *parent = nullptr;  // Parent node or NULL (root)
+
+    normal_base() { }
+    normal_base(const normal_base &other) {
+        data = other.data;
+        stat = other.stat;
+        parent = other.parent;
+    }
 };
 
 struct join_main : virtual normal_base {
@@ -57,21 +73,47 @@ struct join_main : virtual normal_base {
     atomic<node *> neigh2{PREPARING};  // Joined n... (neighbor?)
     node *gparent;                     // Grand parent
     node *otherb;                      // Other branch
+
+    join_main() : normal_base() { }
+    join_main(const join_main &other) : normal_base(other) {
+        neigh1 = other.neigh1;
+        neigh2.store(other.neigh2.load());
+        gparent = other.gparent;
+        otherb = other.otherb;
+    }
 };
 
 struct join_neighbor : virtual normal_base {
     node *main_node;  // The main node for the join
+
+    join_neighbor() : normal_base() { }
+    join_neighbor(const join_neighbor &other) : normal_base(other) {
+        main_node = other.main_node;
+    }
 };
 
 struct rs {                           // Result storage for range queries
     atomic<treap *> result{NOT_SET};  // The result
     atomic<bool> more_than_one_base{false};
+
+    rs() { }
+    rs(const rs &other) {
+        result.store(other.result.load());
+        more_than_one_base.store(other.more_than_one_base.load());
+    }
 };
 
 struct range_base : virtual normal_base {
     int lo;
     int hi;  // Low and high key
     rs *storage;
+
+    range_base() : normal_base() { }
+    range_base(const range_base &other) : normal_base(other) {
+        lo = other.lo;
+        hi = other.hi;
+        storage = other.storage;  // TODO: should this be copied into a new object instead of linking to the same result storage?
+    }
 };
 
 enum node_type {
@@ -84,6 +126,11 @@ enum node_type {
 
 struct node : route_node, range_base, join_main, join_neighbor {
     node_type type;
+
+    node() : route_node(), range_base(), join_main(), join_neighbor() { }
+    node(const node &other) : route_node(other), range_base(other), join_main(other), join_neighbor(other) {
+        type = other.type;
+    }
 };
 
 struct lfcat {
